6e8ecb08f5911e733dfb369290350ad17b6ca857
Opilane TTHK <opilane@tthk.ee>
2021-11-11 10:27:37 +0200

    DataFromFile

---
+++ Chuck/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace Chuck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i > 3; i++)
+            {
+                Console.WriteLine(RandomLineFromFile());
+            }
+            
+        }
+
+        private static string RandomLineFromFile()
+        {
+            string filePath = @"C:\Users\opilane\samples\Chuck.txt";
+            string[] dataFromFile = File.ReadAllLines(filePath);
+
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, dataFromFile.Length);
+
+            return dataFromFile[randomIndex];
+
+            
+        }
+
+
+
+    }
+}
---
+++ Horoscope/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Horoscope
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your horoscope:");
+        }
+    }
+}
